8204e84db99051a461a8618c9c794ac2dee742b3
Michael Hernandez <michael.hernandez27@outlook.com>
2020-05-13 16:43:44 -0400

    reinstalled my other repo that got lost

---
+++ app/data/friends.js
@@ -0,0 +1,305 @@
+
+var friendsArray = [{
+
+    name: 'Mike Trout',
+
+    photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.forbes.com%2Fprofile%2Fmike-trout%2F&psig=AOvVaw1Z3Pt4Hl2XhyGHn5tha_z6&ust=1589397734009000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIj-6pGGr-kCFQAAAAAdAAAAABAD',
+    
+    scores: [
+
+        '1',
+
+        '2',
+
+        '3',
+
+        '4',
+
+        '5',
+
+        '1',
+
+        '2',
+
+        '3',
+
+        '4',
+
+        '5'
+
+    ]
+
+}, {
+
+    name: 'Bryce Harper',
+
+    photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.si.com%2Fmlb%2F2019%2F10%2F18%2Fbryce-harper-washington-nationals-world-series-feelings&psig=AOvVaw2jEfp44pxWT0o4_GfaP0VY&ust=1589397785426000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPCV5a2Gr-kCFQAAAAAdAAAAABAD',
+
+    scores: [
+
+        '5',
+
+        '4',
+
+        '3',
+
+        '2',
+
+        '1',
+
+        '5',
+
+        '4',
+
+        '3',
+
+        '2',
+
+        '1'
+
+    ]
+
+}, {
+
+    name: 'Nolan Arenado',
+
+    photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.si.com%2Fmlb%2F2019%2F08%2F09%2Fnolan-arenado-colorado-rockies&psig=AOvVaw1IPKNJx-JxQVdN-vUltGp_&ust=1589397826768000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKil7ceGr-kCFQAAAAAdAAAAABAD',
+
+    scores: [
+
+        '2',
+
+        '3',
+
+        '4',
+
+        '5',
+
+        '1',
+
+        '2',
+
+        '3',
+
+        '4',
+
+        '5',
+
+        '1'
+
+    ]
+
+}, {
+
+    name: 'Cody Bellinger',
+
+    photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.truebluela.com%2F2019%2F4%2F10%2F18300668%2Flos-angeles-dodgers-cody-bellingers-hot-start-courtesy-of-improved-plate-discipline&psig=AOvVaw2EvNYF2O232vQjP3h92529&ust=1589397881896000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLj70t2Gr-kCFQAAAAAdAAAAABAP',
+
+    scores: [
+
+        '3',
+
+        '4',
+
+        '5',
+
+        '1',
+
+        '2',
+
+        '3',
+
+        '4',
+
+        '5',
+
+        '1',
+
+        '2'
+
+    ]
+
+}, {
+
+    name: 'Christian Yelich',
+
+    photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.fishstripes.com%2F2019%2F1%2F25%2F18195039%2Fbrewers-marlins-worst-trades-christian-yelich-mvp-lewis-brinson-one-year-later&psig=AOvVaw21zotXajNE9c6AROYKc5NN&ust=1589397965891000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMjBqIiHr-kCFQAAAAAdAAAAABAD',
+
+    scores: [
+
+        '4',
+
+        '5',
+
+        '1',
+
+        '2',
+
+        '3',
+
+        '4',
+
+        '5',
+
+        '1',
+
+        '2',
+
+        '3'
+
+    ]
+
+}, {
+
+    name: 'Aaron Judge',
+
+    photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bostonglobe.com%2Fsports%2Fredsox%2F2017%2F11%2F14%2Faaron-judge-wins-rookie-year%2FgXbf1LfLTn4uxCgHA8u2PJ%2Fstory.html&psig=AOvVaw0jMRVTRhKsynICLnHlEncj&ust=1589398015828000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNjGsMWHr-kCFQAAAAAdAAAAABAK',
+
+    scores: [
+
+        '5',
+
+        '1',
+
+        '2',
+
+        '3',
+
+        '4',
+
+        '5',
+
+        '1',
+
+        '2',
+
+        '3',
+
+        '4'
+
+    ]
+
+}, {
+
+    name: 'Anthony Rendon',
+
+    photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nbclosangeles.com%2Fnews%2Fsports%2Fanthony-rendon-goes-2-for-2-in-angels-spring-training-debut%2F2317879%2F&psig=AOvVaw11NPPaEltUgYjXJiwBO-gb&ust=1589398169526000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCODZreSHr-kCFQAAAAAdAAAAABAJ',
+
+    scores: [
+
+        '1',
+
+        '1',
+
+        '1',
+
+        '1',
+
+        '1',
+
+        '1',
+
+        '1',
+
+        '1',
+
+        '1',
+
+        '1'
+
+    ]
+
+}, {
+
+    name: 'Ronald Acuna Jr.',
+
+    photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.si.com%2Fmlb%2F2019%2F10%2F04%2Fronald-acuna-jr-lack-hustle-braves&psig=AOvVaw3Y36cz5IkLMAQfm9H_U3QG&ust=1589398252761000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCi1IyIr-kCFQAAAAAdAAAAABAD',
+
+    scores: [
+
+        '5',
+
+        '5',
+
+        '5',
+
+        '5',
+
+        '5',
+
+        '5',
+
+        '5',
+
+        '5',
+
+        '5',
+
+        '5'
+
+    ]
+
+}, {
+
+    name: 'JD Martinez',
+
+    photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.si.com%2Fmlb%2F2020%2F03%2F05%2Fjd-martinez-opposes-banning-in-game-video&psig=AOvVaw0gY0iJkFoRmh-Hzq8ahNn3&ust=1589398302425000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIip1qmIr-kCFQAAAAAdAAAAABAD',
+
+    scores: [
+
+        '3',
+
+        '3',
+
+        '3',
+
+        '3',
+
+        '3',
+
+        '3',
+
+        '3',
+
+        '3',
+
+        '3',
+
+        '3'
+
+    ]
+
+}, {
+
+    name: 'Javier Baez',
+
+    photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cbssports.com%2Fmlb%2Fnews%2Fwhat-a-javier-baez-extension-could-look-like-as-cubs-reportedly-negotiate-new-deal-with-star-shortstop%2F&psig=AOvVaw1jaPSure1E8oZ3JiBtWb1s&ust=1589398371109000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJj-5b-Ir-kCFQAAAAAdAAAAABAD',
+
+    scores: [
+
+        '4',
+
+        '4',
+
+        '4',
+
+        '4',
+
+        '4',
+
+        '2',
+
+        '2',
+
+        '2',
+
+        '2',
+
+        '2'
+
+    ]
+
+}]
+
+
+module.exports = friendsArray;
